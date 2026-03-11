86400a436bca3c9f46f0dbac0ca8c5dc6d0e4e7a
SpoopyEd <eduardoaguilera1031@gmail.com>
2026-03-10 22:22:50 -0700

    Error catching in ListNode added

--- ListNode.java
+++ ListNode.java
@@ -43,69 +43,73 @@ public class ListNode {
     }
     public int search(String search) {
         int count = 0;
-        ListNode current = front;
-        while (current != null) { // do until there is no next assigned to the node, effectively making it the end of the list
-            //System.out.println("While loop"); // test
-            count += 1;
-            if (search.equals(current.name) || search.equals(current.number) || search.equals(current.address)) { // if search matches name, number, or address
-                System.out.println("Match found");
-                System.out.println();
-                return count;
+        if (front == null) {
+            System.out.println("Empty List Error");
+            return count;
+        } else {
+            ListNode current = front;
+            while (current != null) { // do until there is no next assigned to the node, effectively making it the end of the list
+                count += 1;
+                if (search.equals(current.name) || search.equals(current.number) || search.equals(current.address)) { // if search matches name, number, or address
+                    System.out.println("Match found");
+                    System.out.println();
+                    return count;
+                }
+                current = current.next;
             }
-            //System.out.println("Match not found, onto next"); // test
-            current = current.next;
+            count = 0;
         }
-        count = 0;
         return count;
     }
     public void swap(int index1, int index2) {
-        if (index1 == index2) return; // if they're the same, don't bother
-        ListNode contact1 = getContact(index1);
-        ListNode contact2 = getContact(index2);
-        // updates nodes that are pointing towards given contacts
-        if (contact1.prev != null) {
-            contact1.prev.next = contact2;
-        } else {
-            front = contact2; // if contact 1 was the head
+        try {
+            if (index1 == index2) return; // if they're the same, don't bother
+            ListNode contact1 = getContact(index1);
+            ListNode contact2 = getContact(index2);
+            // updates nodes that are pointing towards given contacts
+            if (contact1.prev != null) {
+                contact1.prev.next = contact2;
+            } else {
+                front = contact2; // if contact 1 was the head
+            }
+            if (contact2.prev != null) {
+                contact2.prev.next = contact1;
+            } else {
+                front = contact1; // if contact 1 was the head
+            }
+            if (contact1.next != null) {
+                contact1.next.prev = contact2;
+            }
+            if (contact2.next != null) {
+                contact2.next.prev = contact1;
+            }
+            ListNode tempNext = contact1.next;
+            ListNode tempPrev = contact1.prev;
+            if (tempNext == contact2) { // if they're next to eachother (contact1 before contact2)
+                contact1.next = contact2.next;
+                contact1.prev = contact2;
+                contact2.next = contact1;
+                contact2.prev = tempPrev;
+            }
+            else if (contact2.next == contact1) { // if they're next to eachother (contact2 before contact1)
+                contact2.next = contact1.next;
+                contact2.prev = contact1;
+                contact1.next = contact2;
+                contact1.prev = contact2.prev;
+            } 
+            else { // if they aren't next to eachother
+                contact1.next = contact2.next;
+                contact1.prev = contact2.prev;
+                contact2.next = tempNext;
+                contact2.prev = tempPrev;
+            }
         }
-        if (contact2.prev != null) {
-            contact2.prev.next = contact1;
-        } else {
-            front = contact1; // if contact 1 was the head
-        }
-        if (contact1.next != null) {
-            contact1.next.prev = contact2;
-        }
-        if (contact2.next != null) {
-            contact2.next.prev = contact1;
-        }
-        ListNode tempNext = contact1.next;
-        ListNode tempPrev = contact1.prev;
-        if (tempNext == contact2) { // if they're next to eachother (contact1 before contact2)
-            contact1.next = contact2.next;
-            contact1.prev = contact2;
-            contact2.next = contact1;
-            contact2.prev = tempPrev;
-        }
-        else if (contact2.next == contact1) { // if they're next to eachother (contact2 before contact1)
-            contact2.next = contact1.next;
-            contact2.prev = contact1;
-            contact1.next = contact2;
-            contact1.prev = contact2.prev;
-        } 
-        else { // if they aren't next to eachother
-            contact1.next = contact2.next;
-            contact1.prev = contact2.prev;
-            contact2.next = tempNext;
-            contact2.prev = tempPrev;
+        catch (Exception e) {
+            System.out.println("Out of bounds error when swapping");
         }
     }
     public void delete(int index) { // deletes a contact and adjusts accordingly
-        // handle negatives
-        if (index > count()) { // add this to other index methods
-            System.out.println("No contact at that index");
-            return;
-        } else {
+        try {
             ListNode current = front;
             for (int i = 1; i < index; i++) {
                 current = current.next;
@@ -123,14 +127,19 @@ public class ListNode {
                 current.next.prev = current.prev; // next that is pointing at deleted now points at what deleted points at
             }
             current.nullify();
+        } catch (Exception e) {
+            System.out.println("Out of bounds error when deleting");
         }
     }
     public ListNode getContact(int index) {
         ListNode current = front;
-        for (int i = 1; i < index; i++) {
-            current = current.next;
+        try {
+            for (int i = 1; i < index; i++) {
+                current = current.next;
+            }
+        } catch (Exception e) {
+            System.out.println("Out of bounds error when getting contact");
         }
-        
         return current;
     }
     public int count() { // counts amount of contacts in the list
@@ -143,14 +152,14 @@ public class ListNode {
         return count;
     }
     public void display(int index) { // displays information of contact at "index"
-        if (index > count()) {
-            System.out.println("No contact at that index");
-        } else {
+        try {
             ListNode current = front;
             for (int i = 1; i < index; i++) {
                 current = current.next;
             }
             System.out.printf("Contact %d%nName: %s%nNumber: %s%nAddress: %s%n%n", index, current.name, current.number, current.address);
+        } catch (Exception e) {
+            System.out.println("Out of bounds error when displaying");
         }
     }
     public void displayAll() { // displays all contacts
